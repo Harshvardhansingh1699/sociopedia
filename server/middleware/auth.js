7dea16aba159a1d28083cf373a2816c159b543f7
Harshvardhansingh1699 <109609830+Harshvardhansingh1699@users.noreply.github.com>
2023-01-16 00:49:51 +0530

    intial commit

---
+++ server/middleware/auth.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+
+export const verifyToken = async (req, res, next) => {
+    try {
+        let token = req.header("Authorization");
+        // console.log("here's the token:", token)
+        if (!token){
+            return res.status(403).send("Access Denied");
+        }
+        if (token.startsWith("Bearer ")) {
+            token = token.slice(7, token.lenght).trimLeft();
+        }
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        // console.log("this is verified var: ", verified);
+        req.user = verified;
+        next();
+    } catch (err) {
+        res.status(500).json({ erroe: err.message });
+    } 
+};
